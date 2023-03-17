dd8a5df2caaae112113feea05662a3ed3dcca252
abidbaig177 <119119875+abidbaig177@users.noreply.github.com>
2023-03-17 20:59:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
-CAPTION = environ.get("CAPTION", "")
+API_ID = int(environ.get("API_ID", "23864794"))
+API_HASH = environ.get("API_HASH", "0176a32f396e45a0a33644f678507d76")
+BOT_TOKEN = environ.get("BOT_TOKEN", "5819273706:AAGTWxjtuisl_9_tZcNxj3-KY663tMXF6Ts")
+ADMIN = int(environ.get("ADMIN", "5181939388"))          
+CAPTION = environ.get("CAPTION", "http://t.me/Video_to_file_rename_bot")
 
 class temp(object):
     THUMBNAIL = environ.get("THUMBNAIL", "")
